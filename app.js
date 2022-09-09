6ae72247727685863c9b18b6ce9ee75726f4535f
Dan Page <dantpage27@gmail.com>
2022-09-09 15:15:20 -0400

    added initial JS

---
+++ app.js
@@ -0,0 +1,22 @@
+const ratings = {
+    rating: "",
+    potentialRatings: document.querySelector(".ratings__list"),
+    button: document.querySelector(".ratings__btn")
+};
+const ratingsNum = document.querySelector(".ratings__number");
+
+const rating = document.querySelector(".rating");
+const thanks = document.querySelector(".thanks");
+
+ratings.potentialRatings.addEventListener('click', function (e) {
+    ratings.score = e.path[0].textContent;
+});
+
+ratings.button.addEventListener('click', function (e) {
+    ratingsNum.textContent = ratings.score;
+
+    rating.style.display = "none";
+    thanks.style.display = "block";
+});
+
+
