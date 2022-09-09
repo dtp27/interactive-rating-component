303a7d20e7b94585b0db55c1f4a78bd136b6b87c
Dan Page <dantpage27@gmail.com>
2022-09-09 17:02:35 -0400

    completed the active state adjustments

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const ratings = {
-    rating: "",
+    score: "",
     potentialRatings: document.querySelector(".ratings__list"),
     button: document.querySelector(".ratings__btn")
 };
@@ -10,13 +10,24 @@ const thanks = document.querySelector(".thanks");
 
 ratings.potentialRatings.addEventListener('click', function (e) {
     ratings.score = e.path[0].textContent;
+    e.path[0].style.backgroundColor = "var(--clr-light-grey)";
+    e.path[0].style.color = "var(--clr-white)";
+
+    /* Make sure previous selection is reset to base color scheme */
+    for (let li of e.path[1].children) {
+        li.style.backgroundColor = "var(--clr-shaded-dark-blue)";
+        li.style.color = "var(--clr-medium-grey)";
+    }
+
+    e.path[0].style.backgroundColor = "var(--clr-light-grey)";
+    e.path[0].style.color = "var(--clr-white)";
 });
 
 ratings.button.addEventListener('click', function (e) {
-    ratingsNum.textContent = ratings.score;
-
-    rating.style.display = "none";
-    thanks.style.display = "flex";
+    if (ratings.score !== "") {
+        ratingsNum.textContent = ratings.score;
+    
+        rating.style.display = "none";
+        thanks.style.display = "flex";
+    }
 });
-
-
